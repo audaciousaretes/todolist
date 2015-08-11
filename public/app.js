df98ca7a9457a654f307b6a626306cbd2e0d058c
Mychelle Blake <mychelle.blake@gmail.com>
2015-08-10 17:08:22 -0700

    Adds revised app.js file for homework assignment.

--- public/app.js
+++ public/app.js
@@ -1,36 +1,88 @@
-var todoList = function () {
-	var list = [];
 
-	this.addTodo = function (description) {
-		list.push(new todo(description));
-	};
+var todolist = function () {
+	var theList = [];
 
-	this.getTodos = function () {
-		return list;
+	this.addTask = function (description) {
+		var newTask = new task(description);
+		theList.push(newTask);
+		createAppendUIHelper(newTask);
 	};
 
 	this.count = function () {
-		return list.length;
+		return theList.length;
+	};
+
+	this.listTasks = function () {
+		return theList;
 	};
 };
 
-var tl = new todoList;
 
-var todo = function (description) {
+var newlist = new todolist;
+
+var task = function (description) {
 	this.description = description;
 	var status = "incomplete";
 
-	this.getStatus = function() {
-		return status;
-	};
-
-	this.complete = function () {
-	status = "complete";
+	this.completed = function () {
+	status = "completed";
 	};
 
 	this.deferred = function() {
 	status = "Deferred";
 	};
+
+	this.getStatus = function() {
+	return status;
+	};
 };
+
+function createTaskUIHelper(task) {
+	var li = document.createElement("li");
+	li.innerHTML = task.description;
+	li.className = task.getStatus();
+	return li;
+}
+
+function appendTaskUIHelper (createdLI) {
+	document.getElementById("todolist").appendChild(createdLI);
+}
+
+function createAppendUIHelper (task) {
+	var taskElement = createTaskUIHelper(task);
+	appendTaskUIHelper(taskElement);
+}
+
+function setupListener () {
+	var formNodes = document.getElementsByTagName("form");
+	var form = formNodes[0];
+	form.addEventListener("submit", function(event) {
+		event.preventDefault();
+		var description = document.getElementById("newtask").value;
+		newlist.addTask(description);
+		document.getElementsById("newtask").value = "";
+	};
+}
+
+setupListener ();
+
+// SECTION #2
+// Create a todo list constructor
+// 	private list
+// 	add todo method
+// 	get todo list length
+// 	show todos
+// Create a todo constructor
+// 	public description
+// 	private status
+// 	get status method
+// 	set status to "complete" method
+// 	set status to "deferred" method
+
+
+
+ 
+
+
 
 
