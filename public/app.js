392ee440fd79d8c0088eb7bf0b6114f1ad355fe1
Nicholas Reynolds <sonshine_80@yahoo.com>
2015-08-20 19:11:36 -0700

    Adds javascript to the todos

--- public/app.js
+++ public/app.js
@@ -1,46 +1,29 @@
 var toDoList = function () {
-	var list = [];
-	this.addToDo = function (description) {
-		var newToDo = new toDo(description);
-		list.push(newToDo);
-		return newToDo;
-	};
-	this.getToDos = function () {
-		return list;
-	};
-	this.count = function () {
-		return list.length;
-	};
+    var list = [];
+    this.addToDo = function (description) {
+        var newToDo = new toDo(description);
+        list.push(newToDo);
+        this.updateLocalTodos();
+        return newToDo;
+    };
+    this.getToDos = function () {
+        return list;
+    };
+    this.count = function () {
+        return list.length;
+    };
+
+    this.updateLocalTodos = function () {
+        var todos = {
+        	description: "",
+     	}
+    };	
+
+    this.getLocalTodos = function () {
+       return localStorage.todos; 
+    };
+
+    this.destroyAllTodos = function () {
+        delete localStorage.todos;
+    };
 };
-var tl = new toDoList();
-
-var toDo = function (description) {
-	this.description = description;
-	var status = "incomplete";
-	this.getStatus = function () {
-		return status;
-	};
-	this.complete = function () {
-		status = "complete";
-	};
-	this.deffered = function () {
-		status = "deffered";
-	};
-};
-
-function createAndAppendToDoUIHelper (toDo) {
-	var tmpl = $("#todo-template").text();
-	var render = _.template(tmpl);
-	$("#todolist").append(render({
-		description: toDo.description,
-		status: toDo.getStatus
-	}));
-}
-
-$("form").on("submit", function(event) {
-	event.preventDefault();
-	var todoType = $("#todoitem").val();
-	var newToDo = tl.addToDo(todoType);
-	$("#todoitem").val("");
-	createAndAppendToDoUIHelper(newToDo);
-});
