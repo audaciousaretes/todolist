832f9509dae75bb4f512e9f61d500044ea1d7557
Mychelle Blake <mychelle.blake@gmail.com>
2015-08-20 19:10:13 -0700

    Adds javascript to todos

--- public/app.js
+++ public/app.js
@@ -1,67 +1,19 @@
-
-var todolist = function () {
-	var theList = [];
-
-	this.addTask = function (description) {
-		var newTask = new task(description);
-		theList.push(newTask);
-		createAppendUIHelper(newTask);
-	};
-
-	this.count = function () {
-		return theList.length;
-	};
-
-	this.listTasks = function () {
-		return theList;
-	};
+var getRemoteTodos = function (callback) {
+    $.getJSON("http://localhost:8080/todos", function(response))
+    callback(response.todos);
+});
 };
 
-
-var tl = new todolist;
-
-var task = function (description) {
-	this.description = description;
-	var status = "incomplete";
-
-	this.completed = function () {
-	status = "completed";
-	};
-
-	this.deferred = function() {
-	status = "Deferred";
-	};
-
-	this.getStatus = function() {
-	return status;
-	};
-};
-
-var todolistElement = $("#todoList");
-
-var todolist = [{
-	description: "Do your homework",
-	status: "incomplete"
-},{
-	description: "Train the evil puppy",
-	status: "incomplete"
-},{
-	description: "Do the dishes",
-	status: "incomplete"
-}];
-
-var templateStr = $("todo-template").text();
-var renderTemplate = _.template(templateStr);
-
-_.each(todoList, function (todo) {
-	var newHtml = renderTemplate(task);
-	todoListElement.append(newHTML);
+getRemoteTodos(function (todos) {
+    var $todolist = $("todoList");
+    var $todoTemplate = $("#todoTemplate").html();
+    todos.forEach(function (todo) {
+        var rendered = _.template($todoTemplate)(todo);
+        $todolist.append(rendered);
+});
 });
 
-$("form").on("submit", function(e) {
-	e.preventDefault();
-	var todoText = $("#createTodoInput").val();
-	todolistElement.append("<li>" + todoText + "</li>");
-});
-
-
+//adding todo
+//deleting todo
+//change the status
+//editing a todo
